299fa4c0044155189d789bc537a4a9300dc5de11
Ahmad-Aburub <129858206+Ahmad-Aburub@users.noreply.github.com>
2023-04-04 16:06:10 +0300

    Update all_sums.py

--- all_sums.py
+++ all_sums.py
@@ -1,15 +1,15 @@
 #Enter Python code here and hit the Run button.
-def get_comb(num_list, i = 0, comb_list = str(), sum = 0):
-    print(type(comb_list))
+def get_comb(num_list, i = 0, comb_list = str(), sum = 0, ans = []):
     if i == (len(num_list)):
-        print(comb_list)
-        return comb_list
-    get_comb(num_list, i + 1, comb_list +f' {str(num_list[i])}', sum + num_list[i])
-    get_comb(num_list, i + 1, comb_list, sum)
-    
-    
-    
+        ans.append([int(x) for x in comb_list.strip().split()])
+        return ans
+    get_comb(num_list, i + 1, comb_list +f' {str(num_list[i])}', sum + num_list[i], ans)
+    get_comb(num_list, i + 1, comb_list, sum, ans)
+    return ans
     
 nums = [int(x) for x in input().split()]
 print(nums)
-get_comb(nums)
+all_combs = get_comb(nums)
+print(all_combs)
+sum_list = {sum(comb) for comb in all_combs if comb}
+print(sum_list)
